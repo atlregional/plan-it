f6e571002205f03f07354260d0e5e4850e109683
Landon Reed <landontreed@gmail.com>
2014-01-15 19:59:14 -0500

    added branch creation to pull request chain

--- javascripts/data.js
+++ javascripts/data.js
@@ -406,10 +406,31 @@ var edit = false
       "body": body
     })
     // $.post("https://api.github.com/repos/landonreed/plan-it/git/refs", data, function(data){console.log(data)})
-    var repo = github.getRepo('landonreed', 'plan-it');
-    var newHead = 'landonreed-tester-1'
-    repo.show(function(err, repo) {console.log(repo)});
     var copy = grid.collection.slice();
+    var newHead = $.cookie('user').login + id.toLowerCase()
+    var repo = github.getRepo('landonreed', 'plan-it');
+    repo.branch('gh-pages', newHead, function(err) {
+      repo.write(newHead, 'data/TIP/individual/'+id+'.csv', JSON2CSV(copy), body, function(err) {
+        console.log(err)
+        var pull = {
+          "title": title,
+          "body": body,
+          "base": "gh-pages",
+          "head": newHead
+        };
+        repo.createPullRequest(pull, function(err, pullRequest) {
+          $(this).button('reset')
+          console.log(pullRequest)
+          $.each(changes, function(i, change){undoChange()})
+          $('#issue-modal-title').html('Success!')
+          $('#modal-edits').hide()
+          $('#issue-modal-success').show()
+          $('#issue-modal-success-link').html('See your issue <a href="' + pullRequest.html_url + '">here</a>.')  
+        });
+      });
+    });
+    repo.show(function(err, repo) {console.log(repo)});
+    
 
     // removes index attribute from grid.collection copy
     // $.each(copy, function(i, row){
@@ -420,24 +441,7 @@ var edit = false
 
     // convert json to csv
     
-    repo.write(newHead, 'data/TIP/individual/'+id+'.csv', JSON2CSV(copy), body, function(err) {
-      console.log(err)
-      var pull = {
-        "title": title,
-        "body": body,
-        "base": "gh-pages",
-        "head": newHead
-      };
-      repo.createPullRequest(pull, function(err, pullRequest) {
-        $(this).button('reset')
-        console.log(pullRequest)
-        $.each(changes, function(i, change){undoChange()})
-        $('#issue-modal-title').html('Success!')
-        $('#modal-edits').hide()
-        $('#issue-modal-success').show()
-        $('#issue-modal-success-link').html('See your issue <a href="' + pullRequest.html_url + '">here</a>.')  
-      });
-    });
+    
     
     // $.post(url, data, function(data){
       
